09baa0f3ec9bf8eb5f4649b39f0c974d669c1fab
Camila <alvescamila87@gmail.com>
2024-08-07 10:36:23 -0300

    add lombok dependency and put lombok in the models: Pessoa and Endereco

--- src/main/java/br/com/camila/cadastropessoa/model/Endereco.java
+++ src/main/java/br/com/camila/cadastropessoa/model/Endereco.java
@@ -2,8 +2,16 @@ package br.com.camila.cadastropessoa.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Endereco {
 	
 	@Id
@@ -18,72 +26,5 @@ public class Endereco {
 	private String gia;
 	private String ddd;
 	private String siafi;
-	
-	public String getCep() {
-		return cep;
-	}
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
-	public String getLogradouro() {
-		return logradouro;
-	}
-	public void setLogradouro(String logradouro) {
-		this.logradouro = logradouro;
-	}
-	public String getComplemento() {
-		return complemento;
-	}
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
-	public String getUnidade() {
-		return unidade;
-	}
-	public void setUnidade(String unidade) {
-		this.unidade = unidade;
-	}
-	public String getBairro() {
-		return bairro;
-	}
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-	public String getLocalidade() {
-		return localidade;
-	}
-	public void setLocalidade(String localidade) {
-		this.localidade = localidade;
-	}
-	public String getUf() {
-		return uf;
-	}
-	public void setUf(String uf) {
-		this.uf = uf;
-	}
-	public String getIbge() {
-		return ibge;
-	}
-	public void setIbge(String ibge) {
-		this.ibge = ibge;
-	}
-	public String getGia() {
-		return gia;
-	}
-	public void setGia(String gia) {
-		this.gia = gia;
-	}
-	public String getDdd() {
-		return ddd;
-	}
-	public void setDdd(String ddd) {
-		this.ddd = ddd;
-	}
-	public String getSiafi() {
-		return siafi;
-	}
-	public void setSiafi(String siafi) {
-		this.siafi = siafi;
-	}
 
 }
--- src/main/java/br/com/camila/cadastropessoa/model/Pessoa.java
+++ src/main/java/br/com/camila/cadastropessoa/model/Pessoa.java
@@ -5,8 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pessoa {
 	
 	@Id
@@ -18,39 +23,5 @@ public class Pessoa {
 	
 	@ManyToOne
 	private Endereco endereco;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNomeCompleto() {
-		return nomeCompleto;
-	}
-	public void setNomeCompleto(String nomeCompleto) {
-		this.nomeCompleto = nomeCompleto;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-	public Endereco getEndereco() {
-		return endereco;
-	}
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
-	}
-	
-	
-	
 
 }
